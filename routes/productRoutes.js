35d4dd9c95d097a2172632dba52eb00882df8bff
Carlos <cde_htm@hotmail.com>
2022-11-23 04:36:50 -0300

    Generacion endpoint modificar prod

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,5 +3,7 @@ const productController = require('../controllers/productController');
 const api = express.Router();
 
 api.post('/product', productController.createProduct);
+api.get('/products', productController.getProducts);
+api.put('/product/update/:id', productController.updateProduct);
 
 module.exports = api
